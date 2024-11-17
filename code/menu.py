317acf0a37d6881b187b150623574a12bc6526dd
denissongomes <denissongomes@outlook.com.br>
2024-11-17 18:15:47 -0300

    Update menu option and event for keys up and down

--- code/menu.py
+++ code/menu.py
@@ -5,7 +5,7 @@ import pygame.image
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE
+from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, COLOR_YELLOW, COLOR_BLUE
 
 
 class Menu:
@@ -24,15 +24,31 @@ class Menu:
 			self.menu_text(50, "Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
 
 			for i in range(len(MENU_OPTION)):
-				self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
-
-			pygame.display.flip()
+				if i == menu_option:
+					self.menu_text(20, MENU_OPTION[i], COLOR_BLUE, ((WIN_WIDTH / 2), 200 + 25 * i))
+				else:
+					self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
 
 			# Check for all events
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					pygame.quit()  # Close window
 					quit()  # End pyganme
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_DOWN: # Down Key
+						if menu_option < len(MENU_OPTION) -1:
+							menu_option += 1
+						else:
+							menu_option = 0
+
+					elif event.key == pygame.K_UP: # Up Key
+						if menu_option > 0:
+							menu_option -= 1
+						else:
+							menu_option = len(MENU_OPTION) -1
+
+
+			pygame.display.flip()
 
 	def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
 		text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
