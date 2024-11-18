fc3dc59e27cb9ac41186f56d709b36da0c741e23
denissongomes <denissongomes@outlook.com.br>
2024-11-18 18:12:46 -0300

    Creating ABC class Updating entityFactory Level 1 Bg implementation: paralax

--- code/level.py
+++ code/level.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
+
 from code.entity import Entity
+from code.entityFactory import EntityFactory
 
 
 class Level:
@@ -8,7 +11,16 @@ class Level:
         self.window = window
         self.name = name
         self.game_mode = game_mode # menu_option
-        self.entity_list = list[Entity] = []
+        self.entity_list: list[Entity] = []
+        self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
 
-    def run(self, ):
+    def run(self):
+        while True:
+            for ent in self.entity_list:
+                self.window.blit(source=ent.surf, dest=ent.rect)
+                ent.move()
+
+            pygame.display.flip()
         pass
+
+
