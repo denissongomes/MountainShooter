7cad83d34455361067233dc670ba4227ab8d841d
Denisson Gomes <denissongomes@outlook.com.br>
2024-11-21 19:12:15 -0300

    Level 2 implementation

--- code/Const.py
+++ code/Const.py
@@ -10,22 +10,28 @@ C_CYAN = (0, 128, 128)
 
 # E
 EVENT_ENEMY = pygame.USEREVENT + 1
+EVENT_TIMEOUT = pygame.USEREVENT + 2
 ENTITY_SPEED = {
-	'Level1Bg0': 0,
-	'Level1Bg1': 1,
-	'Level1Bg2': 2,
-	'Level1Bg3': 3,
-	'Level1Bg4': 4,
-	'Level1Bg5': 5,
-	'Level1Bg6': 6,
-	'Player1': 3,
-	'Player1Shot': 1,
-	'Player2': 3,
-	'Player2Shot': 3,
-	'Enemy1': 1,
-	'Enemy1Shot': 5,
-	'Enemy2': 1,
-	'Enemy2Shot': 2,
+	    'Level1Bg0': 0,
+    'Level1Bg1': 1,
+    'Level1Bg2': 2,
+    'Level1Bg3': 3,
+    'Level1Bg4': 4,
+    'Level1Bg5': 5,
+    'Level1Bg6': 6,
+    'Level2Bg0': 0,
+    'Level2Bg1': 1,
+    'Level2Bg2': 2,
+    'Level2Bg3': 3,
+    'Level2Bg4': 4,
+    'Player1': 3,
+    'Player1Shot': 1,
+    'Player2': 3,
+    'Player2Shot': 3,
+    'Enemy1': 1,
+    'Enemy1Shot': 5,
+    'Enemy2': 1,
+    'Enemy2Shot': 2,
 
 }
 
@@ -125,6 +131,10 @@ PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
 
 # S
 SPAWN_TIME = 4000
+
+# T
+TIMEOUT_STEP = 100  # 100ms
+TIMEOUT_LEVEL = 20000  # 20s
 
 # W
 WIN_WIDTH = 576
--- code/entityFactory.py
+++ code/entityFactory.py
@@ -10,23 +10,26 @@ from code.player import Player
 
 class EntityFactory:
 
-    @staticmethod
-    def get_entity(entity_name: str, position=(0, 0)):
-        match entity_name:
-            case 'Level1Bg': # Backgrounds Level 1
-                list_bg = []
-                for i in range(7):
-                    list_bg.append(Background(f'Level1Bg{i}', (0,0)))
-                    list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
-                return list_bg
-            case 'Player1':
-                return Player('Player1', (10, WIN_HEIGHT / 2 - 30))
-            case 'Player2':
-                return Player('Player2', (10, WIN_HEIGHT / 2 + 30))
-            case 'Enemy1':
-                return Enemy('Enemy1', (WIN_HEIGHT + 10, random.randint(40, WIN_HEIGHT - 40)))
-            case 'Enemy2':
-                return Enemy('Enemy2', (WIN_HEIGHT + 10, random.randint(40, WIN_HEIGHT - 40)))
-
-
-
+	@staticmethod
+	def get_entity(entity_name: str, position=(0, 0)):
+		match entity_name:
+			case 'Level1Bg':  # Backgrounds Level 1
+				list_bg = []
+				for i in range(7):  # level1bg images number
+					list_bg.append(Background(f'Level1Bg{i}', (0, 0)))
+					list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
+				return list_bg
+			case 'Level2Bg':  # Backgrounds Level 2
+				list_bg = []
+				for i in range(5):  # level2bg images number
+					list_bg.append(Background(f'Level2Bg{i}', (0, 0)))
+					list_bg.append(Background(f'Level2Bg{i}', (WIN_WIDTH, 0)))
+				return list_bg
+			case 'Player1':
+				return Player('Player1', (10, WIN_HEIGHT / 2 - 30))
+			case 'Player2':
+				return Player('Player2', (10, WIN_HEIGHT / 2 + 30))
+			case 'Enemy1':
+				return Enemy('Enemy1', (WIN_HEIGHT + 10, random.randint(40, WIN_HEIGHT - 40)))
+			case 'Enemy2':
+				return Enemy('Enemy2', (WIN_HEIGHT + 10, random.randint(40, WIN_HEIGHT - 40)))
