76a159444b45b6d2cc654c47d8f1e4da1d5b1252
Denisson Gomes <denissongomes@outlook.com.br>
2024-11-21 15:45:41 -0300

    Hit Box Collision HUD Score

--- code/menu.py
+++ code/menu.py
@@ -5,7 +5,7 @@ import pygame.image
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, COLOR_YELLOW, COLOR_BLUE
+from code.Const import WIN_WIDTH, C_ORANGE, MENU_OPTION, C_WHITE, C_YELLOW, C_BLUE
 
 
 class Menu:
@@ -21,14 +21,14 @@ class Menu:
 		while True:
 			# Draw images
 			self.window.blit(source=self.surf, dest=self.rect)
-			self.menu_text(50, "Mountain", COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
-			self.menu_text(50, "Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
+			self.menu_text(50, "Mountain", C_ORANGE, ((WIN_WIDTH / 2), 70))
+			self.menu_text(50, "Shooter", C_ORANGE, ((WIN_WIDTH / 2), 120))
 
 			for i in range(len(MENU_OPTION)):
 				if i == menu_option:
-					self.menu_text(20, MENU_OPTION[i], COLOR_BLUE, ((WIN_WIDTH / 2), 200 + 25 * i))
+					self.menu_text(20, MENU_OPTION[i], C_BLUE, ((WIN_WIDTH / 2), 200 + 25 * i))
 				else:
-					self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
+					self.menu_text(20, MENU_OPTION[i], C_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
 			pygame.display.flip()
 
 			# Check for all events
